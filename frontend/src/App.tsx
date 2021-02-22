ec0e6ccdfea31d6bdd2d53a2384a29342226d73f
thangplum <thangplum@gmail.com>
2021-02-22 02:01:38 -0600

    Add a sketch navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,22 @@
+import { Col, Layout, Menu, Row } from 'antd';
+import { Header } from 'antd/lib/layout/layout';
+import SubMenu from 'antd/lib/menu/SubMenu';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        
+        <Menu theme="dark" mode="horizontal">
+          
+            <Menu.Item>Pathfinding</Menu.Item>
+            <Menu.Item>Sorting</Menu.Item>
+          
+          
+        </Menu>
+      </Layout>
     </div>
   );
 }
